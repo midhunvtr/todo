870391754208963b41d82e1905c2dd25fc6f4e00
midhunvtr <midhunvtr@gmail.com>
2021-07-06 08:28:05 +0530

    commit

---
+++ login.js
@@ -0,0 +1,23 @@
+let uname=document.getElementById("uname");
+let password=document.getElementById("pwd");
+
+
+function validate(callback){
+ if(uname.value==""||password.value=="")
+   {
+       alert("Fields cannot be empty");
+       return false;
+   }
+   else if(uname.value=="admin"&&password.value=="12345"){
+     callback();
+   }
+   else{
+    alert("Username or Password is incorrect");
+    return false;
+   }
+}
+
+function v_cal(){
+  alert("Validation is proper");
+  return true;
+}
